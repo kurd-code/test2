fc287bf13f923e878c2e8acce327634dd38a071a
mohamad <mohamad@gmail.com>
2023-03-24 11:46:15 -0700

    add all

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const imag = document.querySelector('.imag')
+const menuL = document.querySelector('.menu-left');
+const menu = document.querySelector('.menu');
+
+function sss(){
+    imag.classList.toggle('active');
+    menuL.classList.toggle('activ');
+}
